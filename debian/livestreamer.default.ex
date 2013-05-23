f5ea23b2f21de4d59c2aac4c7a012464a7f8074f
Philippe Coval <rzr@gna.org>
2013-05-24 00:06:01 +0200

    debian: Initial packaging

---
+++ debian/livestreamer.default.ex
@@ -0,0 +1,10 @@
+# Defaults for livestreamer initscript
+# sourced by /etc/init.d/livestreamer
+# installed at /etc/default/livestreamer by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
